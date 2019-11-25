f63eb8e916dc21064128d132bf3e3f9b5607064e
Sara Caballero Bruno <sara2cb@gmail.com>
2019-11-25 16:07:20 +0000

    Added particles

--- particles.js
+++ particles.js
@@ -1,13 +1,65 @@
 "use strict";
 const anglePoints = 10;
 const angle = 370;
-
+const gravity = 9.8;
 const heightTopCup = 90;
 const heightBottomCup = -100;
-const radiousCup = 50;
+const radiusCup = 50;
 
 var bubbleData;
-var noParticles = 100;
+var noParticles = 600;
+var sideSpeed = 0.2;
+
+const maxTimeOut = 2000;
+
+
+
+function getRandomPositionAndSizeInCup(){
+  var random = Math.random();
+    var edges = [radiusCup/9,radiusCup*(3/4), radiusCup*(4/5), radiusCup];
+    var sizes = [4/10, 8/10, 15/10, 25/10];
+    var maxRadCup = edges[3];
+    var minRadCup = edges[2];
+    var maxRadBubble = sizes[3];
+    var minRadBubble = sizes[2];
+    if(random<4/10){
+      minRadCup = edges[0];
+      maxRadCup = edges[1];
+      maxRadBubble = sizes[1];
+      minRadBubble = sizes[0];
+    }else if(random<9/10){
+      minRadCup = edges[1];
+      maxRadCup = edges[2];
+      maxRadBubble = sizes[2];
+      minRadBubble = sizes[1];
+    }
+
+    var u = Math.random()+Math.random();
+    var theta = Math.random() * 2 * Math.PI
+    var r = u;
+    if(u>1){ r = 2-u; }
+    else{ r = u;}
+
+    if(r<minRadCup){ r = minRadCup+r*((maxRadCup-minRadCup)/minRadCup);}
+
+    var centerX = r * Math.sin(theta)
+    var centerZ = r * Math.cos(theta)
+    var radiusBubble =  Math.random() * (maxRadBubble - minRadBubble) + minRadBubble;
+
+    return [centerX, centerZ, radiusBubble];
+}
+
+function closestCirclePoint(Cx, Cy, Px, Py, rad){
+    var den = Math.sqrt((Px-Cx)*(Px-Cx)+(Py-Cy)*(Py-Cy));
+    var Rx = Cx + rad * ((Px-Cx)/den);
+    var Ry = Cy + rad * ((Py-Cy)/den);
+    return[Rx, Ry]
+}
+
+function velocity(p,R,v){
+  return (2/9)*((p*gravity*R*2)/v);
+}
+
 function radian (degree) {
   var rad = degree * (Math.PI / 180);
   return rad;
@@ -37,30 +89,21 @@ function main() {
 
   bubbleData = new Array(noParticles);
 
-  var show = true;
-
+  var bubblePoints = createCirclePoints({x: 0, y: heightBottomCup, z:-360}, 1, 0, 0, 0, {x: 0, y:100, z:200});
+  
   for(var i = 0; i<noParticles; i++){
     bubbleData[i] = {};
 
-    var r = (radiousCup-radiusBubble) * Math.sqrt(Math.random())
-    var theta = Math.random() * 2 * Math.PI
-    var centerX = r * Math.sin(theta)
-    var centerZ = r * Math.cos(theta)
+    var values = getRandomPositionAndSizeInCup();
+    var centerX = values[0];
+    var centerZ = values[1];
 
-    var placeInGlass  =Math.sqrt(centerX*centerX + centerZ*centerZ);
-    var radiusBubble = 2;
+    var radiusBubble = values[2];
 
-    if(placeInGlass < radiousCup/4){
-      radiusBubble = 0.5;
-    }else if(placeInGlass < (radiousCup*(3/4))){
-      radiusBubble = 1;
-    }
     // Create a buffer to put positions in
     bubbleData[i]["PositionBuffer"] = gl.createBuffer();
     // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = bubblePositionBuffer)
     gl.bindBuffer(gl.ARRAY_BUFFER, bubbleData[i]["PositionBuffer"]);
-
-    var bubblePoints = createCirclePoints({x: centerX, y: heightBottomCup + radiusBubble, z:centerZ-360}, radiusBubble, 0, 0, 0, {x: 0, y:100, z:200});+
 
     // Put geometry data into buffer
     setBubbleGeometry(gl, bubblePoints);
@@ -72,9 +115,13 @@ function main() {
     // Put geometry data into buffer
     setBubbleColors(gl, bubblePoints);
 
-    bubbleData[i]["locations"] = [centerX, centerZ];
+    bubbleData[i]["locations"] = [centerX, radiusBubble, centerZ];
+    bubbleData[i]["radius"] = radiusBubble;
+    bubbleData[i]["sideSpeed"] = 0.2;
+    bubbleData[i]["timeOut"] = -1
   }
 
+  console.log(bubbleData);
   //CUP---------------------------------------------------------------------------------------------------
 
   var cupsInfo = new Array(2);
@@ -87,10 +134,10 @@ function main() {
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = bubblePositionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, cupsInfo[1]['posbuf']);
 
-  var cupBigPoints = createCupPoints(radiousCup+10, heightTopCup+20, heightBottomCup-10, [200,200,200]);
+  var cupBigPoints = createCupPoints(radiusCup+10, heightTopCup+20, heightBottomCup-10, [200,200,200], true);
 
   // Put geometry data into buffer
-  setCupGeometry(gl, cupBigPoints);
+  setCupGeometry(gl, cupBigPoints.p);
 
 
   // Create a buffer to put colors in
@@ -98,7 +145,7 @@ function main() {
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, cupsInfo[1]['colbuf']);
   // Put geometry data into buffer
-  setCupColors(gl, cupBigPoints);
+  setCupColors(gl, cupBigPoints.c);
 
   cupsInfo[0] = {};
 
@@ -107,17 +154,17 @@ function main() {
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = bubblePositionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, cupsInfo[0]['posbuf']);
 
-  var cupPoints = createCupPoints(radiousCup, heightTopCup, heightBottomCup, [0,100,200]);
+  var cupPoints = createCupPoints(radiusCup, heightTopCup, heightBottomCup, [0,100,200], false);
 
   // Put geometry data into buffer
-  setCupGeometry(gl, cupPoints);
+  setCupGeometry(gl, cupPoints.p);
 
   // Create a buffer to put colors in
   cupsInfo[0]['colbuf'] = gl.createBuffer();
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, cupsInfo[0]['colbuf']);
   // Put geometry data into buffer
-  setCupColors(gl, cupPoints);
+  setCupColors(gl, cupPoints.c);
 
 
   function radToDeg(r) {
@@ -128,10 +175,10 @@ function main() {
     return d * Math.PI / 180;
   }
 
-  var translation = [10, 0, 0];
-  var rotation = [degToRad(190), degToRad(40), degToRad(320)];
+  var translation = [0, 0, 0];
   var fieldOfViewRadians = degToRad(60);
   var rotationSpeed = 0.7;
+  
 
   drawScene();
 
@@ -161,21 +208,16 @@ function main() {
     gl.useProgram(program);
 
 
-    cupShape(cupsInfo[1]);
-    cupShape(cupsInfo[0]);
-
-    translation[0] = translation[0] + rotationSpeed
-    if(translation[0] > heightTopCup-radiusBubble){
-      translation[0] = heightBottomCup + radiusBubble;
-    }
+    cupShape(cupsInfo[1], ((angle/anglePoints) * 3) + 5);
+    cupShape(cupsInfo[0], ((angle/anglePoints) * 4) + 4);
 
     for(var i = 0; i<howMany; i++){
       animationCircle(bubbleData[i], bubbleData[i]["PositionBuffer"], bubbleData[i]["ColourBuffer"]);
     }
 
     counter++;
-    if(counter == 50 && howMany< noParticles){
-      howMany += noParticles/20;
+    if(counter == 1 && howMany< noParticles){
+      howMany += 1;
       if(howMany>noParticles){
         howMany = noParticles-1;
       }
@@ -187,9 +229,31 @@ function main() {
 
   function animationCircle(data, bubblePositionBuffer, colorBubbleBuffer){
 
-    data["locations"][1] = data["locations"][1] + rotationSpeed
-    if( data["locations"][1] > (heightTopCup-radiusBubble)*2){
-      data["locations"][1] = 0;
+    
+    if( data["locations"][1] > (heightTopCup-heightBottomCup)){
+      var today = new Date();
+      if( data["timeOut"] < 0){
+        data["timeOut"] = 0
+        data["lastCounted"] = today.getMilliseconds();
+      }else if(data["timeOut"]<maxTimeOut*(data["radius"]/2.5)){
+        var now = today.getMilliseconds();
+        if(now<=data["lastCounted"]){
+          data["timeOut"] += data["lastCounted"]-now;
+        }else{
+          data["timeOut"] += 59-data["lastCounted"] + now;
+        }
+        data["lastCounted"] = today.getMilliseconds();
+      }
+      else{
+        data["locations"][1] = 0;
+        var values = getRandomPositionAndSizeInCup();
+        data["locations"][0] = values[0];
+        data["locations"][2] = values[1];
+        data["radius"] = values[2];
+        data["timeOut"] = -1
+      }
+    }else{
+      data["locations"][1] = data["locations"][1] + velocity(1,data["radius"],1.51)/2.5
     }
 
     // Turn on the position attribute
@@ -226,7 +290,8 @@ function main() {
     var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     var matrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
     
-    matrix = m4.translate(matrix, translation[1],  data["locations"][1], translation[2]);
+    matrix = m4.translate(matrix, (data["locations"][0]*radiusCup)/(radiusCup-data["radius"]*3),  data["locations"][1], data["locations"][2]);
+    matrix = m4.scale(matrix, data["radius"], 1,1);
     gl.uniformMatrix4fv(matrixLocation, false, matrix);
 
     
@@ -239,7 +304,7 @@ function main() {
   }
 
 
-  function cupShape(cups){
+  function cupShape(cups, noLines){
 
     // Turn on the position attribute
     gl.enableVertexAttribArray(positionLocation);
@@ -280,8 +345,7 @@ function main() {
     // Draw the geometry.
     var primitiveType = gl.LINES;
     var offset = 0;
-    var count = ((angle/anglePoints) * 3) + 5;
-    gl.drawArrays(primitiveType, offset, count);
+    gl.drawArrays(primitiveType, offset, noLines);
 
   }
 
@@ -373,39 +437,37 @@ function setBubbleColors(gl, initialPoints) {
       gl.STATIC_DRAW);
 }
 
-function createCupPoints(radius, heightTop, heightBottom, colour){
+function createCupPoints(radius, heightTop, heightBottom, colour, noBack){
   
   var base = createCirclePoints({x: 0, y: heightBottom, z:0}, radius, 90, 0, 0, {x: colour[0], y:colour[1], z:colour[2]});
 
   var top = createCirclePoints({x: 0, y: heightTop, z:0}, radius, 90, 0, 0, {x: colour[0], y:colour[1], z:colour[2]});
 
-  console.log(base);
-
   var noPoints = (base.p.length/3);
 
-  var iniPoint = Math.round(noPoints/4)*3+3;
-  var midPoint = noPoints*3-Math.round(noPoints/4)*3+3;
-  var finPoint = (noPoints)*3;
+  var iniPoint = Math.round(noPoints/4)*3;
+  var midPoint = Math.round(noPoints/4)*3;
+  var secondPoint = noPoints*3-Math.round(noPoints/4)*3+3;
 
-  console.log(noPoints);
-  console.log(iniPoint);
-  console.log(midPoint);
-  console.log(finPoint);
+  if(noBack){
+    
 
-  var behp = base.p.slice(iniPoint,  midPoint);
-  var behc = base.c.slice(iniPoint,  midPoint);
-  base.p = base.p.slice(0, iniPoint).concat(base.p.slice(midPoint, finPoint));
-  base.c = base.c.slice(0, iniPoint).concat(base.c.slice(midPoint, finPoint));
+    iniPoint = Math.round(noPoints/4)*3+3;
+    midPoint = noPoints*3-Math.round(noPoints/4)*3+3;
+    var finPoint = (noPoints)*3;
+    secondPoint = iniPoint-3;
 
-  console.log(base);
+    // var behp = base.p.slice(iniPoint,  midPoint);
+    // var behc = base.c.slice(iniPoint,  midPoint);
+    base.p = base.p.slice(0, iniPoint).concat(base.p.slice(midPoint, finPoint));
+    base.c = base.c.slice(0, iniPoint).concat(base.c.slice(midPoint, finPoint));
+  }
 
   var final = {p:base.p.concat(top.p), c:base.c.concat(top.c)};
-
-  console.log(final);
+  
   final.p.push(base.p[iniPoint], base.p[iniPoint+1], base.p[iniPoint+2]);
   final.p.push(top.p[midPoint], top.p[midPoint+1], top.p[midPoint+2]);
 
-  var secondPoint = iniPoint-3;
   final.p.push(base.p[secondPoint], base.p[secondPoint+1], base.p[secondPoint+2]);
   final.p.push(top.p[secondPoint], top.p[secondPoint+1], top.p[secondPoint+2]);
 
@@ -423,17 +485,17 @@ function setCupGeometry(gl, cupPoints) {
   gl.bufferData(
       gl.ARRAY_BUFFER,
       new Float32Array(
-        cupPoints.p
+        cupPoints
         ),
       gl.STATIC_DRAW);
 }
 
 // Fill the buffer with colors for the 'F'.
-function setCupColors(gl, cupPoints) {
+function setCupColors(gl, cupCol) {
   gl.bufferData(
       gl.ARRAY_BUFFER,
       new Uint8Array(
-        cupPoints.c
+        cupCol
         ),
       gl.STATIC_DRAW);
 }
